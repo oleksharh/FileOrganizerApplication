c0b7bed064bc5eebc4b673e037f20446dd380da1
oleksharh <oleksharh@gmail.com>
2024-04-27 19:54:53 +0100

    second commit

--- app/button_functions.py
+++ app/button_functions.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QFileDialog
 from app.organizer import modify_dest, define_chosen_directories, modify_src, create_folders, move_files
 from app.ui_modifications import MainWindow
+import os
 
 class ButtonFunctions(MainWindow):
     def __init__(self):
@@ -28,34 +29,29 @@ class ButtonFunctions(MainWindow):
             self.label_description.setText(f"Moving from {new_value_src} to {new_value_dest}")
 
     def submit_create_directories(self):
-        
-        checked_buttons = [
-            self.c_gallery.isChecked(),
-            self.c_zip.isChecked(),
-            self.c_docs.isChecked(),
-            self.c_codes.isChecked(),
-            self.c_3d_files.isChecked(),
-            self.c_apps.isChecked()
-        ]
+        if os.path.isdir(self.dest_path_line_edit.text()):
+            checked_buttons = [
+                self.c_gallery.isChecked(),
+                self.c_zip.isChecked(),
+                self.c_docs.isChecked(),
+                self.c_codes.isChecked(),
+                self.c_3d_files.isChecked(),
+                self.c_apps.isChecked()
+            ]
 
-        new_value_src = self.src_path_line_edit.text()
-        modify_src(new_value_src)
-
-        new_value_dest = self.dest_path_line_edit.text()
-        modify_dest(new_value_dest)
-        
-        self.src_path_line_edit.clear()
-        self.dest_path_line_edit.clear()
-
-        self.label_description.setText(f"Moving from {new_value_src} to {new_value_dest}")
-
-        define_chosen_directories(checked_buttons)
-
-        create_folders()
-
-        self.label_result_create.setText("Folders have been successfuly created")
+            define_chosen_directories(checked_buttons)
+            create_folders()
+            
+            self.label_result_create.setText("Folders have been successfuly created")
+        else:
+            self.label_result_create.setText("Define destination directory")
+            
 
     def run_move_files(self):
-        self.label_result_organize.setText("Completed Successfuly")
-
-        move_files()
+        if os.path.isdir(self.src_path_line_edit.text()) and os.path.isdir(self.dest_path_line_edit.text()):
+            move_files()
+            self.label_result_organize.setText("Completed Successfuly")
+        else:
+            self.label_result_organize.setText("Define source and destination directories!!!")
+            
+        
