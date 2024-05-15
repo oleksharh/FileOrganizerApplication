050bacc32c54c6eae800a32e66c9f5e4be871f72
oleksharh <oleksharh@gmail.com>
2024-05-15 23:28:37 +0100

    improved readability

--- app/button_functions.py
+++ app/button_functions.py
@@ -1,5 +1,11 @@
 from PyQt5.QtWidgets import QFileDialog
-from app.organizer import modify_dest, define_chosen_directories, modify_src, create_folders, move_files
+from app.organizer import (
+    modify_dest,
+    define_chosen_directories,
+    modify_src,
+    create_folders,
+    move_files,
+)
 from app.app_logic import MainWindow
 import os
 from PyQt5.QtCore import QTimer
@@ -7,12 +13,21 @@ from PyQt5.QtCore import QTimer
 button_error = "background-color: #FF0000; color: white; font-weight: bold;"
 button_success = "background-color: green; color: white; font-weight: bold;"
 
+
 class ButtonFunctions(MainWindow):
     def __init__(self):
         super().__init__()
-        
-        self.browse_and_submit_button_1.clicked.connect(lambda: self.browse_button_func(self.src_path_line_edit, "Choose Source Directory"))
-        self.browse_and_submit_button_2.clicked.connect(lambda: self.browse_button_func(self.dest_path_line_edit, "Choose Destination Directory"))   
+
+        self.browse_and_submit_button_1.clicked.connect(
+            lambda: self.browse_button_func(
+                self.src_path_line_edit, "Choose Source Directory"
+            )
+        )
+        self.browse_and_submit_button_2.clicked.connect(
+            lambda: self.browse_button_func(
+                self.dest_path_line_edit, "Choose Destination Directory"
+            )
+        )
         self.submit_button1.clicked.connect(self.submit_create_directories)
         self.submit_button2.clicked.connect(self.run_move_files)
         self.tick_all_button.clicked.connect(self.tick_all)
@@ -37,49 +52,73 @@ class ButtonFunctions(MainWindow):
                 self.c_docs.isChecked(),
                 self.c_codes.isChecked(),
                 self.c_3d_files.isChecked(),
-                self.c_apps.isChecked()
+                self.c_apps.isChecked(),
             ]
 
             define_chosen_directories(checked_buttons)
             create_folders()
-            
-            self.output_submit_button(self.submit_button1, "Successfully Created", button_success)
-            QTimer.singleShot(1300, lambda: self.reset_submit_button(self.submit_button1))
+
+            self.output_submit_button(
+                self.submit_button1, "Successfully Created", button_success
+            )
+            QTimer.singleShot(
+                1300, lambda: self.reset_submit_button(self.submit_button1)
+            )
         else:
-            self.output_submit_button(self.submit_button1, "Define destination directory!!!", button_error)
-            QTimer.singleShot(1300, lambda: self.reset_submit_button(self.submit_button1))
+            self.output_submit_button(
+                self.submit_button1, "Define destination directory!!!", button_error
+            )
+            QTimer.singleShot(
+                1300, lambda: self.reset_submit_button(self.submit_button1)
+            )
 
     def tick_all(self):
-        checkbox_list = [self.c_gallery, self.c_zips, self.c_docs, self.c_codes, self.c_3d_files, self.c_apps]
+        checkbox_list = [
+            self.c_gallery,
+            self.c_zips,
+            self.c_docs,
+            self.c_codes,
+            self.c_3d_files,
+            self.c_apps,
+        ]
+        
         all_checked = all(check_box.isChecked() for check_box in checkbox_list)
 
         if all_checked:
             for checkbox in checkbox_list:
                 checkbox.setChecked(False)
-            self.tick_all_button.setText('Tick All')
+            self.tick_all_button.setText("Tick All")
         else:
             for checkbox in checkbox_list:
                 checkbox.setChecked(True)
-            self.tick_all_button.setText('Untick All')
+            self.tick_all_button.setText("Untick All")
 
     def run_move_files(self):
-        if os.path.isdir(self.src_path_line_edit.text()) and os.path.isdir(self.dest_path_line_edit.text()):
+        if os.path.isdir(self.src_path_line_edit.text()) and os.path.isdir(
+            self.dest_path_line_edit.text()
+        ):
             move_files(self.dest_path_line_edit.text())
-            
-            self.output_submit_button(self.submit_button2, "Completed Successfully", button_success)
-            QTimer.singleShot(1300, lambda: self.reset_submit_button(self.submit_button2))
+
+            self.output_submit_button(
+                self.submit_button2, "Completed Successfully", button_success
+            )
+            QTimer.singleShot(
+                1300, lambda: self.reset_submit_button(self.submit_button2)
+            )
         else:
-            self.output_submit_button(self.submit_button2, "Define directories!!!", button_error)
-            QTimer.singleShot(1300, lambda: self.reset_submit_button(self.submit_button2))
+            self.output_submit_button(
+                self.submit_button2, "Define directories!!!", button_error
+            )
+            QTimer.singleShot(
+                1300, lambda: self.reset_submit_button(self.submit_button2)
+            )
 
     def reset_submit_button(self, button):
         button.setStyleSheet("")
         button.setEnabled(True)
         button.setText("Submit")
-        
+
     def output_submit_button(self, button, text, stylesheet):
         button.setText(text)
         button.setEnabled(False)
         button.setStyleSheet(stylesheet)
-        
-        
